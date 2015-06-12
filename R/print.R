d657af9c3c055e1962249bc65dfeb7939ef3ea62
todeschi <adrien.todeschini@inria.fr>
2015-06-12 16:41:38 +0200

    ameliore print_tab, print_taches + utilise longtables

--- R/print.R
+++ R/print.R
@@ -2,55 +2,93 @@
 #' Print table to latex
 #' @param df data.frame
 #'
+#' @param envir string. table environment to be used in latex
 #' @param col_format string. column format
-#' @param rowcol string. row color
+#' @param rowcolors NULL or list with fields \code{begin} (integer. starting row including header)
+#'   \code{even} (string. even rows color) and \code{odd} (string. odd rows color)
+#' @param rowcol_head string. row color of the columns header
+#' @param format_head string. header columns format
 #' @param hline logical. activate horizontal lines
-#' @param replace_na function or NULL
-#' @param header character vector or NULL. header
+#' @param replace_na function or NULL. replace NA
+#' @param header character vector or NULL. columns header text
+#' @param replace_nl function or NULL. replace newlines
+#' @param vspace_before string or NULL. height of vertical space before the table
+#' @param vspace_after string or NULL. height of vertical space after the table
 #'
 #' @export
 #' @importFrom tidyr unite_
 #' @importFrom dplyr mutate_each
 #' @examples  
 #' print_tab(mtcars)
-print_tab <- function(df, col_format="", rowcol = NULL, hline=TRUE, 
+print_tab <- function(df = data.frame(), 
+                      envir = c("longtable", "tabular", "tabularx"), 
+                      col_format = "", 
+                      rowcolors = NULL,
+                      rowcol_head = "lightgray", hline=TRUE, 
+                      format_head = "\\centering\\bf",
                       replace_na = function(x) ifelse(is.na(x), "", x),
-                      replace_nl = function(x) gsub("\n", " \\\\newline ", x),
-                      header = NULL) {
-  cat("\\begin{tabular}{", col_format, "}\n", sep="")
+                      replace_nl = function(x) gsub("\n", "\\\\newline ", x),
+                      header = colnames(df),
+                      vspace_before = "-.8em",
+                      vspace_after = "-.8em") {
+  envir = match.arg(envir, c("longtable", "tabular", "tabularx"))
+  
+  if (!is.null(vspace_before)) {
+    cat("\\vspace*{", vspace_before, "}\n", sep="")
+  }
+    
+  # begin table
+  if (!is.null(rowcolors)) {
+    cat("\\rowcolors{", rowcolors$begin, "}{", rowcolors$even, "}{", rowcolors$odd, "}\n", sep="")
+  }
+  
+  cat("\\begin{", envir, "}{", col_format, "}\n", sep="")
   
   if (hline)
     cat(" \\hline\n")
   
-  if (!is.null(rowcol))
-    cat("\\rowcolor{", rowcol, "} ", sep="")
-  
-  collapse = " \\tabularnewline\n"
-  if (hline)
-    collapse = paste(collapse, "\\hline\n")
-  
+  # header
   if (!is.null(header)) {
     df_head = data.frame(as.list(header), stringsAsFactors = FALSE)
     df_head = mutate_each(df_head, funs(replace_na))
-    cat(unlist(tidyr::unite_(df_head, "x", colnames(df_head), sep = " & ")))
+    
+    if (!is.null(rowcol_head))
+      cat("\\rowcolor{", rowcol_head, "} ", sep="")
+    
+    cat(format_head,
+        unlist(tidyr::unite_(df_head, "x", colnames(df_head), sep = paste0(" & ", format_head, " "))))
+    
     cat(" \\tabularnewline\n")
     if (hline)
       cat(" \\hline\n")
   }
   
-  if (!is.null(replace_na)) {
-    df = mutate_each(df, funs(replace_na))
+  # content of the table
+  if (nrow(df)>0) {
+    if (!is.null(replace_na)) {
+      df = mutate_each(df, funs(replace_na))
+    }
+    if (!is.null(replace_nl)) {
+      df = mutate_each(df, funs(replace_nl))
+    }
+    
+    collapse = " \\tabularnewline\n"
+    if (hline)
+      collapse = paste(collapse, "\\hline\n")
+    
+    cat(paste(unlist(tidyr::unite_(df, "x", colnames(df), sep = " & ")), collapse = collapse), sep="")
+    
+    if (hline)
+      cat(" \\tabularnewline\n \\hline\n")
   }
-  if (!is.null(replace_nl)) {
-    df = mutate_each(df, funs(replace_nl))
+  
+  # end table
+  
+  cat("\\end{", envir, "}\n", sep="")
+  if (!is.null(vspace_after)) {
+    cat("\\vspace*{", vspace_after, "}\n", sep="")
   }
-  
-  cat(paste(unlist(tidyr::unite_(df, "x", colnames(df), sep = " & ")), collapse = collapse))
-  
-  if (hline)
-    cat(" \\tabularnewline\n \\hline\n")
-  
-  cat("\\end{tabular}\n\\vspace*{.5em}\n\n")
+  cat("\n")
 }
 
 #' Print tasks to latex
@@ -58,15 +96,13 @@ print_tab <- function(df, col_format="", rowcol = NULL, hline=TRUE,
 #' @param taches data.frame
 #' @param legende data.frame
 #' @param section string. section
-#' @param col_type character vector. column types
-#' @param col_sep string. column separator
-#' @param col_form character vector. column format
-#' @param col_form_head character vector. header column format
+#' @param col_format string. column format
 #' @param rowcol_head row color of the header
 #' @param header character vector. header
 #' @param cle_var string. key column name
 #' @param classe_var string. class column name
 #' @param format_fun list. formatting functions
+#' @param ... further arguments to be passed to print_tab
 #'
 #' @export
 #' @importFrom dplyr %>%
@@ -75,28 +111,22 @@ print_tab <- function(df, col_format="", rowcol = NULL, hline=TRUE,
 #' @importFrom dplyr mutate
 #' @importFrom dplyr mutate_each
 #' @importFrom dplyr funs
-print_taches <- function(taches, legende, section = "orga", 
-                         col_type = c("p{11cm}", "p{2cm}", "p{2cm}"),
-                         col_sep = "|",
-                         col_form = c("", ">{\\centering}", ">{\\centering}"),
-                         col_form_head = c(">{\\centering\\bf}", ">{\\centering\\bf}", ">{\\centering\\bf}"),
+print_taches <- function(taches,
+                         legende, section = "orga", 
+                         col_format = c("|p{11cm}|>{\\centering}p{2cm}|>{\\centering}p{2cm}|"),
                          rowcol_head = "lightgray",
-                         header,
+                         header = c("TACHE", "ECHEANCE", "ETAT"),
                          cle_var = "CLE", classe_var = "CLASSE",
-                         format_fun) {
+                         format_fun, ...) {
   ind = match(section, legende[[cle_var]])
   cat("\\section*{", toupper(legende$Nom[ind]), "}\n\n", sep="")  
   
   taches = taches %>% 
     filter(SECTION == section)
   
-  col_format_head = paste0(col_sep, paste0(col_form_head, col_type, collapse=col_sep), col_sep)
-  col_format = paste0(col_sep, paste0(col_form, col_type, collapse=col_sep), col_sep)
-  
-  cat("\\parindent=0em\n")
-  print_tab(data.frame(as.list(header), stringsAsFactors = FALSE), 
-            col_format = col_format_head, 
-            rowcol = rowcol_head)
+  print_tab(header = header,
+            col_format = col_format, 
+            rowcol_head = rowcol_head, ...)
   
   # acteurs dans l'ordre de la legende
   acteurs = unique(taches$ACTEUR)
@@ -145,7 +175,7 @@ print_taches <- function(taches, legende, section = "orga",
       taches_d = taches_d %>% 
         mutate_each(funs(rowfun))
       
-      print_tab(taches_d, col_format=col_format)
+      print_tab(taches_d, col_format=col_format, header=NULL, ...)
     }
   }
 }
@@ -156,16 +186,15 @@ print_taches <- function(taches, legende, section = "orga",
 #' @param col_format string. column format
 #' @param width string. photo max width
 #' @param height string. photo max height
+#' @param ... further arguments to be passed to print_tab
 #'
 #' @export
 #' @importFrom dplyr mutate
-#' @importFrom dplyr %>%
 print_photos = function(photos, col_format="|>{\\centering}m{10cm}|m{7cm}|", 
                        width = "10cm",
-                       height="5cm") {
-  photos = photos %>% 
-    mutate(FICHIER = paste0("\\includegraphics[height=", height, ", width=", width, 
+                       height="5cm", ...) {
+  photos = mutate(photos, FICHIER = paste0("\\includegraphics[height=", height, ", width=", width, 
                             ", keepaspectratio]{", tools::file_path_sans_ext(FICHIER), "}"))
   
-  print_tab(photos, col_format=col_format)
+  print_tab(photos, col_format=col_format, ...)
 }
